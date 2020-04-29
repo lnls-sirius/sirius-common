86245704f8bba86720839c2a8aaf130e24950c90
carneirofc <claudiofcarneiro@hotmail.com>
2020-04-29 13:34:09 -0300

    rename siriushlacommon to conscommon

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from setuptools import setup, find_namespace_packages
-from siriushlacommon import __version__, __author__
+from conscommon import __version__, __author__
 
 requirements = None
 with open("requirements.txt", "r") as _f:
@@ -10,8 +10,8 @@ setup(
     name="siriushlacommon",
     version=__version__,
     author=__author__,
-    description="Commons for Sirius",
-    packages=find_namespace_packages(include=["siriushlacommon", "siriushlacommon.*"]),
+    description="CON Sirius commons",
+    packages=find_namespace_packages(include=["conscommon", "conscommon.*"]),
     license="GNU GPLv3",
     include_package_data=True,
     install_requires=requirements,
