100aafcfef7496eb5eb24e11962cfd976e9dde34
carneirofc <claudiofcarneiro@hotmail.com>
2021-08-03 16:02:53 -0300

    fix scripts imports

--- scripts/get-disconnected-pvs.py
+++ scripts/get-disconnected-pvs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from src import getCurrentlyDisconnectedPVs
+from siriuscommon.archiver import getCurrentlyDisconnectedPVs
 
 if __name__ == "__main__":
     getCurrentlyDisconnectedPVs()
